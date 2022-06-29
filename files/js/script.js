2b5692ac25e148fb2a10446143747a8771d5dae1
Alexandr-Zaburiannyi <ozaburiannyi@gmail.com>
2022-06-29 17:02:53 +0300

    modified application functionalty

--- files/js/script.js
+++ files/js/script.js
@@ -1,7 +1,30 @@
 const pullDataBtn = document.querySelector(".btn");
 
+const table = document.querySelector(".table");
+
+let arrayOfUsers = [];
+
+
+
 pullDataBtn.addEventListener("click",() => {
+    table.classList.remove('d-none');
     pullData();
+    arrayOfUsers.forEach(user => {
+        table.insertAdjacentHTML("beforeend", 
+        `<tr class="table-light">
+                <th scope="row">${user.id}</th>
+                <td>${user.first_name}</td>
+                <td>${user.last_name}</td>
+                <td>${user.email}</td>
+                <td>${user.city}</td>
+                <td>${user.company}</td>
+                <td></td>
+                <td></td>
+            </tr>
+        `)
+    })
+    // let firstUser = arrayOfUsers[0];
+    // console.log(firstUser)
 })
 
 
@@ -9,5 +32,7 @@ pullDataBtn.addEventListener("click",() => {
 async function pullData() {
 const pulledData = await fetch("https://gist.githubusercontent.com/Alexandr-Zaburyannyi/ba8150b7428999fb93aa4497b2dc5c70/raw/f42aa3bb2f78bbcecf17c8895970ec24a02c150b/CRUD.json");
     const json = await pulledData.json();
-    console.log(json);
+    json.forEach(user => {
+       arrayOfUsers.push(user);
+    })
 }
