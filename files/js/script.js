8fc837972e74caa017b1bd81f0a02c7f4bce2628
Alexandr-Zaburiannyi <ozaburiannyi@gmail.com>
2022-06-29 16:13:27 +0300

    html file was styled with bootstrap and some fetch manpulations was added

--- files/js/script.js
+++ files/js/script.js
@@ -1,4 +1,13 @@
-function pullData() {
-    fetch("https://gist.githubusercontent.com/Alexandr-Zaburyannyi/ba8150b7428999fb93aa4497b2dc5c70/raw/f42aa3bb2f78bbcecf17c8895970ec24a02c150b/CRUD.json")
-    .then(response => console.log(response.json));
+const pullDataBtn = document.querySelector(".btn");
+
+pullDataBtn.addEventListener("click",() => {
+    pullData();
+})
+
+
+
+async function pullData() {
+const pulledData = await fetch("https://gist.githubusercontent.com/Alexandr-Zaburyannyi/ba8150b7428999fb93aa4497b2dc5c70/raw/f42aa3bb2f78bbcecf17c8895970ec24a02c150b/CRUD.json");
+    const json = await pulledData.json();
+    console.log(json);
 }
