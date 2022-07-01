e33933fb5be48e4f9bb656a15f17fd3dea128d12
Alexandr-Zaburiannyi <ozaburiannyi@gmail.com>
2022-07-01 15:20:39 +0300

    added some functionality modifications

--- files/js/script.js
+++ files/js/script.js
@@ -2,7 +2,17 @@ const pullDataBtn = document.querySelector(".btn");
 
 const table = document.querySelector(".table");
 
+const tableRows =  document.getElementsByClassName('.table-light');
+
 const addRowButton = document.querySelector(".btn-success");
+
+const addRowSubmitButton = document.querySelector("#submit");
+
+const removeRowButtons = document.getElementsByClassName('btn-danger');
+
+const editRowButtons = document.getElementsByClassName('btn-warning');
+
+const inputGroup = document.querySelectorAll("[aria-label=info]");
 
 let arrayOfUsers = [];
 
@@ -22,7 +32,7 @@ pullDataBtn.addEventListener("click",async () => {
             <td>${user.email}</td>
             <td>${user.city}</td>
             <td>${user.company}</td>
-            <td><button type="button" class="btn  btn-warning btn-sm">edit</button></td>
+            <td><button type="button" class="btn btn-warning btn-sm">edit</button></td>
             <td><button type="button" class="btn btn-danger btn-sm">remove</button></td>
         </tr>`)
     });
@@ -32,9 +42,53 @@ pullDataBtn.addEventListener("click",async () => {
 
 
 async function pullData() {
-const pulledData = await fetch("https://gist.githubusercontent.com/Alexandr-Zaburyannyi/ba8150b7428999fb93aa4497b2dc5c70/raw/f42aa3bb2f78bbcecf17c8895970ec24a02c150b/CRUD.json");
+    const pulledData = await fetch("https://gist.githubusercontent.com/Alexandr-Zaburyannyi/ba8150b7428999fb93aa4497b2dc5c70/raw/f42aa3bb2f78bbcecf17c8895970ec24a02c150b/CRUD.json");
     const json = await pulledData.json();
     json.forEach(user => {
        arrayOfUsers.push(user);
     })
 };
+
+
+
+addRowButton.addEventListener('click' , () => {
+    inputGroup.forEach(input => {
+        input.classList.toggle('d-none');
+    })
+    addRowSubmitButton.classList.toggle('d-none');
+});
+
+
+
+addRowSubmitButton.addEventListener('click', () => {
+
+table.insertAdjacentHTML('beforeend',
+    `<th scope="row">${arrayOfUsers.length + 1}</th>
+        <td>${document.getElementById('name').value}</td>
+        <td>${document.getElementById('surname').value}</td>
+        <td>${document.getElementById('email').value}</td>
+        <td>${document.getElementById('city').value}</td>
+        <td>${document.getElementById('company').value}</td>
+        <td><button type="button" class="btn btn-warning btn-sm">edit</button></td>
+        <td><button type="button" class="btn btn-danger btn-sm">remove</button></td>
+    </tr>`);
+
+    arrayOfUsers.length++; 
+    inputGroup.forEach(input => {
+        input.classList.toggle('d-none');
+    })
+    addRowSubmitButton.classList.toggle('d-none');
+});
+
+
+
+table.addEventListener('click' , () => {
+    for (let i = 0; i < arrayOfUsers.length; i++) {
+        if(event.target === removeRowButtons[i]) {
+            removeRowButtons[i].parentElement.parentElement.remove();
+        };
+    };
+});
+
+
+
