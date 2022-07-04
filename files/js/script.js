ebd4c480fd376faa263f25fb9c0469e192a7145f
Alexandr-Zaburiannyi <ozaburiannyi@gmail.com>
2022-07-04 14:12:51 +0300

    some security additions

--- files/js/script.js
+++ files/js/script.js
@@ -2,8 +2,6 @@ const pullDataBtn = document.querySelector(".btn");
 
 const table = document.querySelector(".table");
 
-const tableRows =  document.getElementsByClassName('.table-light');
-
 const addRowButton = document.querySelector(".btn-success");
 
 const addRowSubmitButton = document.querySelector("#submit-new-row");
@@ -18,27 +16,22 @@ const inputGroup = document.querySelectorAll("[aria-label=info]");
 
 const userInfoCell = document.getElementsByClassName('user-info-cell');
 
-let arrayOfUsers = [];
+(function() {
+
+let userLength;
+
+let positions;
 
 
 
-pullDataBtn.addEventListener("click",async () => {
+pullDataBtn.addEventListener("click", async () => {
     pullDataBtn.classList.add('d-none');
     pullDataBtn.parentElement.classList.add('d-none');
-    await pullData();
+    const users = await pullData();
+    userLength = users.length;
     addRowButton.classList.remove('d-none');
-    arrayOfUsers.forEach(user => {
-        table.insertAdjacentHTML("beforeend", 
-        `<tr class="table-light" id="${user.id}">
-            <th scope="row">${user.id}</th>
-            <td class="user-info-cell">${user.first_name}</td>
-            <td class="user-info-cell">${user.last_name}</td>
-            <td class="user-info-cell">${user.email}</td>
-            <td class="user-info-cell">${user.city}</td>
-            <td class="user-info-cell">${user.company}</td>
-            <td><button type="button" class="btn btn-warning btn-sm">edit</button></td>
-            <td><button type="button" class="btn btn-danger btn-sm">remove</button></td>
-        </tr>`)
+    users.forEach(user => {
+        addRow(user);
     });
     table.classList.remove('d-none');
 });
@@ -47,11 +40,8 @@ pullDataBtn.addEventListener("click",async () => {
 
 async function pullData() {
     const pulledData = await fetch("https://gist.githubusercontent.com/Alexandr-Zaburyannyi/ba8150b7428999fb93aa4497b2dc5c70/raw/f42aa3bb2f78bbcecf17c8895970ec24a02c150b/CRUD.json");
-    const json = await pulledData.json();
-    json.forEach(user => {
-       arrayOfUsers.push(user);
-    })
-};
+    return await pulledData.json();
+}
 
 
 
@@ -71,8 +61,8 @@ addRowButton.addEventListener('click' , () => {
 addRowSubmitButton.addEventListener('click', () => {
 
 table.insertAdjacentHTML('beforeend',
-    `<tr class="table-light" id="${arrayOfUsers.length + 1}">
-    <th scope="row">${arrayOfUsers.length + 1}</th>
+    `<tr class="table-light" id="${userLength + 1}">
+        <th scope="row">${userLength + 1}</th>
         <td class="user-info-cell">${document.getElementById('name').value}</td>
         <td class="user-info-cell">${document.getElementById('surname').value}</td>
         <td class="user-info-cell">${document.getElementById('email').value}</td>
@@ -82,7 +72,7 @@ table.insertAdjacentHTML('beforeend',
         <td><button type="button" class="btn btn-danger btn-sm">remove</button></td>
     </tr>`);
 
-    arrayOfUsers.length++; 
+    userLength++; 
     inputGroup.forEach(input => {
         input.classList.toggle('d-none');
     })
@@ -92,31 +82,31 @@ table.insertAdjacentHTML('beforeend',
 
 
 table.addEventListener('click' , () => {
-    for (let i = 0; i < arrayOfUsers.length; i++) {
+    for (let i = 0; i < userLength; i++) {
         if(event.target === removeRowButtons[i]) {
             removeRowButtons[i].parentElement.parentElement.remove();
         };
     };
 });
 
-let positions;
 
 table.addEventListener('click', () => {
-   for(button of editRowButtons){
+   for (button of editRowButtons) {
         if (event.target === button) {
-        addRowSubmitButton.classList.add('d-none');
 
-        editRowSubmitButton.classList.toggle('d-none');
+            addRowSubmitButton.classList.add('d-none');
 
-        let children =  button.parentElement.parentElement.getElementsByClassName('user-info-cell');
+            editRowSubmitButton.classList.toggle('d-none');
 
-        positions = children;
+            let children =  button.parentElement.parentElement.getElementsByClassName('user-info-cell');
 
-        editData(children);
+            positions = children;
 
-        inputGroup.forEach(input => {
-            input.classList.toggle('d-none');
-        });
+            editData(children);
+
+            inputGroup.forEach(input => {
+                input.classList.toggle('d-none');
+            });
         }
     }
 });
@@ -124,15 +114,18 @@ table.addEventListener('click', () => {
 
 
 function editData(tableRow) {
-        for (var i = 0; i < inputGroup.length; i++) {
-            inputGroup[i].value = tableRow[i].innerHTML;
-        }
-};
+    inputGroup.forEach((input, index) => {
+        input.value = tableRow[index].innerHTML;
+    });
+    // for (let i = 0; i < inputGroup.length; i++) {
+      //  inputGroup[i].value = tableRow[i].innerHTML;
+    //}
+}
 
 
 
 editRowSubmitButton.addEventListener('click' , () => {
-    for (var i = 0; i < inputGroup.length; i++) {
+    for (let i = 0; i < inputGroup.length; i++) {
         positions[i].innerText = inputGroup[i].value;
     }
     inputGroup.forEach(input => {
@@ -140,3 +133,20 @@ editRowSubmitButton.addEventListener('click' , () => {
     });
     editRowSubmitButton.classList.add('d-none');
 });
+
+
+
+function addRow(user) {
+    table.insertAdjacentHTML("beforeend", 
+        `<tr class="table-light" id="${user.id}">
+            <th scope="row">${user.id}</th>
+            <td class="user-info-cell">${user.first_name}</td>
+            <td class="user-info-cell">${user.last_name}</td>
+            <td class="user-info-cell">${user.email}</td>
+            <td class="user-info-cell">${user.city}</td>
+            <td class="user-info-cell">${user.company}</td>
+            <td><button type="button" class="btn btn-warning btn-sm">edit</button></td>
+            <td><button type="button" class="btn btn-danger btn-sm">remove</button></td>
+        </tr>`);
+}
+})();
